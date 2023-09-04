3b377c41f3d6428aa0235eb52ad451b50be0bb87
cesarifuentes <fuentesivancesar@gmail.com>
2023-09-04 15:04:00 -0500

    Created pages and added logo

--- las-huastecas-app/src/components/Header.jsx
+++ las-huastecas-app/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../assets/logo.svg";
 
 const Header = () => {
   return (
@@ -8,7 +9,8 @@ const Header = () => {
         className="container flex flex-wrap items-center justify-between w-full mx-auto"
       >
         <div className="flex items-center pl-4 logo-wrapper">
-          <img src="" alt="" />
+          <img src={logo} alt="logo" />
+          <h6>las huastecas</h6>
         </div>
         <div className="flex items-center justify-between space-x-10 nav-menu-wrapper">
           <div>About</div>
