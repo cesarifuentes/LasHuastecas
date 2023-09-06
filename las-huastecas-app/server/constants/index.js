7d2e2a7e232b8b24dae039d80955032b28350958
cesarifuentes <fuentesivancesar@gmail.com>
2023-09-05 20:14:12 -0500

    Initialized back end application

---
+++ las-huastecas-app/server/constants/index.js
@@ -0,0 +1,81 @@
+export const products = [
+  {
+    id: "product-1",
+    name: "T'ikidh Pak'ak'",
+    description:
+      "3 corn tortillas filled with a cactus, tomato, and onion salsa.",
+    price: 5.0,
+    image: "https://i.postimg.cc/BZ5dqtxZ/tacos.jpg",
+    category: "Tacos",
+  },
+  {
+    id: "product-2",
+    name: "Estrujadas",
+    description:
+      "Crushed up corn tortillas covered in chile, pork lard, beans, and cheese. Served with avocado.",
+    price: 6.0,
+    image: "https://i.postimg.cc/Lsrrs1C5/estrujadas.jpg",
+    category: "Platters",
+  },
+  {
+    id: "product-3",
+    name: "Bocoles",
+    description:
+      "5 thick corn tortillas filled with chilis and beans. Served with avocado and cheese.",
+    price: 4.0,
+    image: "https://i.postimg.cc/fTCnvkLq/bocoles.jpg",
+    category: "Breakfast",
+  },
+  {
+    id: "product-4",
+    name: "Tamales Verdes de Cominos",
+    description:
+      "3 Chicken tamales jalapeño, cumin, cilantro and green tomato.",
+    price: 5.0,
+    image: "https://i.postimg.cc/mkSqV6b1/tamales.jpg",
+    category: "Platters",
+  },
+  {
+    id: "product-5",
+    name: "Zacahuil",
+    description:
+      "Baked corn masa filled with chile, chicken, and pork. Served with pickled vegetables.",
+    price: 6.0,
+    image: "https://i.postimg.cc/D0hHrKKK/zacahuil.jpg",
+    category: "Platters",
+  },
+  {
+    id: "product-6",
+    name: "Jacubes con Camaron",
+    description: "Shrimp and cactus in a chili-based soup.",
+    price: 8.0,
+    image: "https://i.postimg.cc/pLcwg7KD/camarones.jpg",
+    category: "Broth",
+  },
+  {
+    id: "product-7",
+    name: "Mole de Guajolote",
+    description: "Turkey mole served with rice.",
+    price: 8.0,
+    image: "https://i.postimg.cc/TwgB53F9/mole.jpg",
+    category: "Platters",
+  },
+  {
+    id: "product-8",
+    name: "Enchiladas Huastecas",
+    description:
+      "3 corn tortillas covered in tomato, red chile, and green chile sauce. Served with cecina, beans, and a salad.",
+    price: 8.0,
+    image: "https://i.postimg.cc/DzW9FgcR/enchiladas.jpg",
+    category: "Platters",
+  },
+  {
+    id: "product-9",
+    name: "Tlapanilli de Castilán",
+    description:
+      "Catinlán beans made with chile, avocado leaves, and corn masa.",
+    price: 4.0,
+    image: "https://i.postimg.cc/bwvXV8Sy/frijoles.jpg",
+    category: "Broth",
+  },
+];
